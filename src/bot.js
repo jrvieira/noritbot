af4b21d3626debdca58a5a106ad382741b724861
jrvieira <github@jrvieira.com>
2020-12-09 10:07:36 +0000

    clean up

--- src/bot.js
+++ src/bot.js
@@ -246,11 +246,11 @@ function aoc_cooldown () {
 
 const { exec } = require('child_process')
 
+const addr = 'https://adventofcode.com/2020/leaderboard/private/view/983136.json'
+const sess = '53616c7465645f5fc4e12522d9980eeabf2be187fc95f706679b4348d021b749adc5e3b59142db293423708e7913505b'
+const comm = 'curl -s --cookie "session='+sess+'" '+addr
+
 function aoc_leaderboard (ctx) {
-   
-   const addr = 'https://adventofcode.com/2020/leaderboard/private/view/983136.json'
-   const sess = '53616c7465645f5fc4e12522d9980eeabf2be187fc95f706679b4348d021b749adc5e3b59142db293423708e7913505b'
-   const comm = 'curl -s --cookie "session='+sess+'" '+addr
    
    exec (comm, (error, stdout, stderr) => {
       if (error) {
@@ -331,10 +331,6 @@ function aoc_leaderboard (ctx) {
 }
 
 function aoc_time (ctx) {
-   
-   const addr = 'https://adventofcode.com/2020/leaderboard/private/view/983136.json'
-   const sess = '53616c7465645f5fc4e12522d9980eeabf2be187fc95f706679b4348d021b749adc5e3b59142db293423708e7913505b'
-   const comm = 'curl -s --cookie "session='+sess+'" '+addr
    
    exec (comm, (error, stdout, stderr) => {
       if (error) {
