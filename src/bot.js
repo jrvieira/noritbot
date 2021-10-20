a238884bc0598085dc0cee8dc620892d62dceb46
jrvieira <github@jrvieira.com>
2021-10-20 01:13:04 +0000

    oct 2021oct 2021

--- src/bot.js
+++ src/bot.js
@@ -10,14 +10,14 @@ bot.command('debug', debug)
 async function debug (ctx) {
 
    try {
-   
+
       let data = await ctx.getChatAdministrators(ctx.message.chat.id)
 
       data = data.filter(u => !u.user.is_bot).map(u => u.custom_title)
       console.info('* debug', data)
 
    } catch (e) {
-   
+
       console.error(e)
 
    } finally {
@@ -25,23 +25,23 @@ async function debug (ctx) {
       console.info('* ctx.message', ctx.message)
       //console.info('* title', await title(ctx))
       //console.info('* reply', bot.telegram.sendMessage)
-   
+
    }
-   
+
 }
 
 
 async function title (ctx) {
 
    try {
-   
+
       let data = await ctx.getChatAdministrators(ctx.message.chat.id)
 
       return data.filter(u => u.user.id == ctx.message.from.id)
                  .map(u => u)[0].custom_title || ctx.message.from.first_name
 
    } catch (e) {
-   
+
       return ctx.message.from.first_name
 
    }
@@ -55,7 +55,7 @@ bot.hears(/^[Oo]+i+[\s\?]*.*$/, ctx => oi() ? ctx.reply('oi?') : null)
 let oi_t = 0
 let oi_n = 0
 
-function oi () { 
+function oi () {
 
    let now = Date.now()
    console.info('oi?')
@@ -77,27 +77,27 @@ bot.command('pessoas', pessoas)
 async function pessoas (ctx) {
 
    let r
-   
+
    try {
 
-      let admins = await ctx.getChatAdministrators(ctx.message.chat.id)    
+      let admins = await ctx.getChatAdministrators(ctx.message.chat.id)
       admins = admins.filter(u => !u.user.is_bot).map(u => u.custom_title)
 
       r = 'pessoas: \n'
 
       for (let admin of admins) {
-   
+
          r += '\n' + admin
 
       }
 
    } catch (e) {
-    
+
       r = ':('
       console.error(e)
-      
+
    } finally {
-   
+
       ctx.reply(r)
 
    }
@@ -114,7 +114,7 @@ let beer_mem = bot.mem.load('beer')
 async function beer (ctx) {
 
    let r
-   
+
    try {
 
       let admins = await ctx.getChatAdministrators(ctx.message.chat.id)
@@ -125,8 +125,8 @@ async function beer (ctx) {
       let text = ctx.message.text.split(' ')
       let to = text[1].toLowerCase()
       let n = text[2] ? +text[2] : 1
-      
-      if (isNaN(n) || !admins.includes(to) || from === to || n < 0 || n > 20) {
+
+      if (isNaN(n) || !admins.includes(to) || from === to || n < 0 || n > 10 || n !== Math.ceil(n) ) {
 
          r = from + ' fns'
 
@@ -142,12 +142,12 @@ async function beer (ctx) {
       }
 
    } catch (e) {
-    
+
       r = ':('
       console.error(e)
-      
+
    } finally {
-   
+
       ctx.reply(r)
 
    }
@@ -157,20 +157,20 @@ async function beer (ctx) {
 async function beerstat (ctx) {
 
    let r
-   
+
    try {
-   
-      let admins = await ctx.getChatAdministrators(ctx.message.chat.id)    
+
+      let admins = await ctx.getChatAdministrators(ctx.message.chat.id)
       admins = admins.filter(u => !u.user.is_bot).map(u => u.custom_title)
       let from = await ctx.getChatMember(ctx.message.from.id)
       from = from.custom_title
-      
+
       if (!from) throw new Error('no group admins here')
 
       let calc = {}
 
       for (let admin of admins) {
-      
+
          let debt = (beer_mem?.[from]?.[admin] || 0) - (beer_mem?.[admin]?.[from] || 0)
 
          if (debt) calc[admin] = debt
@@ -178,28 +178,28 @@ async function beerstat (ctx) {
       }
 
       if (Object.keys(calc).length) {
-      
+
          r = from + ' beer:\n\n'
 
          for (let admin in calc) {
-   
+
             r += admin + ' ' + calc[admin] + '\n'
 
          }
 
       } else {
-   
+
          r = 'nada'
 
       }
 
    } catch (e) {
-   
+
       r = ':('
       console.error(e)
 
    } finally {
-   
+
       ctx.reply(r)
 
    }
@@ -214,21 +214,21 @@ bot.command('quote', quote)
 let mem_quote = bot.mem.load('quotes')
 
 async function quote (ctx) {
-   
+
    let r
 
    try {
-   
+
       let rand = await mem_quote[Math.random() * mem_quote.length >> 0]
       r = rand.quote + '\n\n' + '- ' + rand.author
 
    } catch (e) {
-   
+
       r = ':('
       console.error(e)
 
    } finally {
-   
+
       ctx.reply(r)
 
    }
@@ -261,7 +261,7 @@ const aoc_sess = '53616c7465645f5f9ad0532e17fb0aa29e9fd49e33bbebbcc268d7cfd20a94
 const aoc_comm = 'curl -s --cookie "session='+aoc_sess+'" '+aoc_addr
 
 function aoc_leaderboard (ctx) {
-   
+
    exec (aoc_comm, (error, stdout, stderr) => {
       if (error) {
          return run(':(')
@@ -273,77 +273,77 @@ function aoc_leaderboard (ctx) {
       }
       return run(JSON.parse(stdout))
    })
-   
+
    function run (data) {
-   
+
       let $ = Object.values
-   
+
       let fmt = {
          hour: 'numeric',
          minute: 'numeric',
          hour12: false,
          timeZone: 'Europe/Lisbon'
       }
-   
+
       let members = []
       let records = {}
-   
+
       for (let member of $(data.members)) {
-   
+
          members.push(member.name)
-   
+
          for (let day in member.completion_day_level) {
-   
+
             records[day] = records[day] || {}
-   
+
             records[day][member.name] = $(member.completion_day_level[day]).map(x =>
                new Intl.DateTimeFormat("pt-PT",fmt).format(new Date(+x.get_star_ts*1000))
             )
-   
+
          }
-   
+
       }
-   
+
       let print = ''
-   
+
       for (let day in records) {
-   
+
          print += '\n' + day.padStart(2,' ')
-   
+
          for (let member of members) {
             records[day][member] = records[day][member] || ['','']
          }
-   
+
          for (let member of members) {
             print += '   ' + (records[day][member][0] || '     ')
          }
-   
+
          print += '\n'
          print += '  '
-   
+
          for (let member of members) {
             print += '   ' + (records[day][member][1] || '     ')
          }
-   
+
          print += '\n'
-   
+
       }
-   
+
       print += '\n'
       print += ' '
-   
+
       for (let member of members) {
          print += '     ' + member.substring(0,3) + ''
       }
 
       ctx.replyWithHTML('<code>' + print + '\n' + '</code>')
-   
-   }  
-   
+
+   }
+
 }
 
 function aoc_time (ctx) {
-   
+
    exec (aoc_comm, (error, stdout, stderr) => {
       if (error) {
          return run(':(')
@@ -355,11 +355,11 @@ function aoc_time (ctx) {
       }
       return run(JSON.parse(stdout))
    })
-   
+
    function run (data) {
-   
+
       let $ = Object.values
-   
+
       let fmt = {
          hour: 'numeric',
          minute: 'numeric',
@@ -368,21 +368,21 @@ function aoc_time (ctx) {
          hour12: false,
          timeZone: 'Europe/Lisbon'
       }
-   
+
       let members = []
       let records = {}
-   
+
       for (let member of $(data.members)) {
-   
+
          members.push(member.name)
-   
+
          for (let day in member.completion_day_level) {
-   
+
             records[day] = records[day] || {}
-            
+
             let p1 = $(member.completion_day_level[day])[0] || null
             let p2 = $(member.completion_day_level[day])[1] || null
-            
+
             if (p1 && p2) {
 
                let d = p2.get_star_ts - p1.get_star_ts
@@ -394,38 +394,38 @@ function aoc_time (ctx) {
 
                records[day][member.name] = dhrs + dmns + dscs
 
-            } 
+            }
 
          }
-   
+
       }
-   
-      let print = '' 
-   
+
+      let print = ''
+
       for (let day in records) {
-   
+
          print += '\n' + day.padStart(2,' ')
-   
+
          for (let member of members) {
             print += '  ' + (records[day][member] || '        ')
          }
-   
+
          print += '\n'
          print += ' '
-   
+
       }
-   
+
       print += '\n'
       print += ' '
-   
+
       for (let member of members) {
          print += '       ' + member.substring(0,3) + ''
       }
 
       ctx.replyWithHTML('<code>' + print + '\n' + '</code>')
-   
-   }  
-   
+
+   }
+
 }
 
 
@@ -446,14 +446,14 @@ function cd () {
 
       if (horas < 48) {
 
-         bot.telegram.setChatTitle(-1001245137014,'Countdown: ' + horas + (horas === 1 ? ' hora ❤️' : ' horas ❤️'))
+         bot.telegram.setChatTitle(-1001245137014, horas + (horas === 1 ? ' hora 🤍' : ' horas 🤍'))
 
       } else if (horas % 24 === 0) {
-         
-         bot.telegram.setChatTitle(-1001245137014,'Countdown: ' + (horas / 24) + ' dias ❤️')
+
+         bot.telegram.setChatTitle(-1001245137014, (horas / 24) + ' dias 🤍')
 
       }
-      
+
    }
 
 }
@@ -486,7 +486,7 @@ async function js (ctx) {
    function run (r) {
 
       ctx.reply(r)
-   
+
    }
 
 }
@@ -519,7 +519,7 @@ async function hs (ctx) {
    function run (r) {
 
       ctx.reply(r)
-   
+
    }
 
 }
@@ -552,7 +552,7 @@ async function py (ctx) {
    function run (r) {
 
       ctx.reply(r)
-   
+
    }
 
 }
@@ -631,7 +631,7 @@ async function covid (ctx) {
       }
       // this package has terrible error handling
       // if (stderr) {
-      //    return run(':(') 
+      //    return run(':(')
       //    console.error(stderr)
       // }
       return run(stdout)
@@ -650,11 +650,11 @@ diario: ${j[1]['Cases (today)'].toLocaleString('eu')} / ${j[0]['Cases (today)'].
 
 ativos: ${j[1]['Active'].toLocaleString('eu')} / ${j[0]['Active'].toLocaleString('eu')}
 graves: ${j[1]['Critical'].toLocaleString('eu')} / ${j[0]['Critical'].toLocaleString('eu')}
-mortes: ${j[1]['Deaths (today)'].toLocaleString('eu')} / ${j[0]['Deaths (today)'].toLocaleString('eu')} 
+mortes: ${j[1]['Deaths (today)'].toLocaleString('eu')} / ${j[0]['Deaths (today)'].toLocaleString('eu')}
 </code>`
 
       ctx.replyWithHTML(r)
-   
+
    }
 
 }
@@ -667,7 +667,7 @@ bot.command('remind', remind)
 async function remind (ctx) {
 
    let caller = await title(ctx)
-   
+
    let reminder = 0
 
    let expr = ctx.message.text.split(' ').slice(1)
@@ -703,16 +703,16 @@ async function remind (ctx) {
 
    let origin = Date.now()
 
-   function format (ts) { 
+   function format (ts) {
 
       let date = new Date(ts)
 
       let [month, day, hour, minute] = [date.getMonth(), date.getDate(), date.getHours(), date.getMinutes()]
-      
+
       let months = ['Jan','Feb','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
-      
+
       return months[month] + ' ' + day + ' ' + (hour).toString().padStart(2,'0') + ':' + (minute).toString().padStart(2,'0')
-   
+
    }
 
    // setReminder is a better setTimeout without 32bit signed int limitation
@@ -740,9 +740,9 @@ async function remind (ctx) {
       console.info('reminding @', reminder, ctx.message.message_id)
 
    } else {
-   
+
       return run(':(')
-   
+
    }
 
    function run (r) {
