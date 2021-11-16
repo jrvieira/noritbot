3dc415f9033fef868a596604d1584d0b5947d0cc
jrvieira <github@jrvieira.com>
2021-11-16 14:52:26 +0000

    /words

--- src/bot.js
+++ src/bot.js
@@ -867,11 +867,7 @@ async function etqlccm (ctx) {
             tags: query
          })
 
-         let save = bot.mem.save('etqlccm',
-            mem_etqlccm
-         )
-
-         bot.mem.save(save)
+         bot.mem.save('etqlccm', mem_etqlccm)
 
       } catch (e) {
 
@@ -922,6 +918,200 @@ async function etqlccm (ctx) {
          ctx.replyWithHTML(r)
 
       }
+
+   }
+
+}
+
+
+// words
+
+bot.command('words', words)
+
+let words_dict
+
+fs.readFile('mem/words.txt', 'utf8', function (err, data) {
+
+   if (err) { throw new Error(err) }
+
+   words_dict = new Set(data.split('\n'))
+
+})
+
+let w = '' // letters
+
+let t // time start
+
+let scores
+
+let done = []
+
+let words_mem = bot.mem.load('words')
+
+async function words (ctx) {
+
+   let query = ctx.message.text.split(' ').slice(1).join(' ')
+
+   if (w) {
+
+      ctx.replyWithHTML('<code>' + [...w].join(' ') + '</code>')
+
+   } else if (query === 'top') {
+
+
+      let scoreboard = ''
+
+      let score_pad = Object.values(words_mem).reduce((a,b) => a.toString().length > b.toString().length ? a : b, 0).toString().length
+
+      for (let score of Object.entries(words_mem).sort((a,b) => a[1] > b[1] ? -1 : 1)) {
+
+         scoreboard += score[1].toString().padStart(score_pad,' ') + ' ' + score[0] + '\n'
+
+      }
+
+      ctx.replyWithHTML('<b>HI SCORES!</b>\n\n' + '<code>' + scoreboard + '</code>')
+
+   } else if (!query || !isNaN(query)) {
+
+      scores = new Map()
+
+      done = []
+
+      let duration = 60 * 1000
+
+      let valid = function (p) {
+
+         console.info(p, 'dict', words_dict.has(p))
+
+         if (p.length < 5 || !words_dict.has(p)) return false
+
+         let m = new Map()
+         let ww = [...w]
+
+         let x
+
+         while (x = ww.pop()) {
+
+            m.set(x,m.has(x) ? m.get(x) + 1 : 1)
+
+         }
+
+         let pp = [...p]
+
+         let y
+
+         while (y = pp.pop()) {
+
+            let v = m.get(y)
+
+            if (v) {
+
+               m.set(y,m.get(y) - 1)
+
+            } else {
+
+               return false
+
+            }
+
+         }
+
+         return true
+
+      }
+
+      function val (p) {
+
+         return 2 ** (p.length - 5)
+
+      }
+
+      duration = query ? query * 1000 : duration
+
+      t = Date.now()
+
+      w = ''
+
+      while (w.length < 16) {
+
+         w += (1 + Math.random()).toString(36).substr(0,6).replace(/[^a-z]+/g,'').replace(/[kwy]+/g,'')
+
+         let vowels = ['a','e','i','o','u']
+
+         w += vowels[Math.random() * vowels.length >> 0]
+
+      }
+
+      w = w.substr(0,16).toUpperCase()
+
+      ctx.replyWithHTML('<b>WORDS! ' + duration / 1000 + 's</b>\n\n<code>' + [...w].join(' ') + '</code>')
+
+      console.info('waiting for answers on', w)
+
+      bot.on('message', async ctx => {
+
+         let p = (ctx.message?.text || '').toUpperCase()
+
+         if ((Date.now() - t) < duration && valid(p) && !done.includes(p)) {
+
+            done.push(p)
+
+            console.info(p, 'valid')
+
+            let caller = await title(ctx)
+
+            if (!scores.has(caller)) scores.set(caller,0)
+
+            scores.set(caller, scores.get(caller) + val(p))
+
+            ctx.reply(caller + ': ' + scores.get(caller))
+
+         }
+
+      })
+
+      setTimeout(async () => {
+
+         if (done.length) {
+
+            for (let score of scores) {
+
+               let s = score[1]
+               let h = words_mem[score[0]] || 0
+
+               if (s > h) {
+
+                  words_mem[score[0]] = s
+                  ctx.reply(score[0] + ': hi score!')
+
+               }
+
+            }
+
+            bot.mem.save('words', words_mem)
+
+            let scoreboard = ''
+
+            let score_pad = Object.values(scores).reduce((a,b) => a.toString().length > b.toString().length ? a : b, 0).toString().length
+
+            for (let score of [...scores].sort((a,b) => a[1] > b[1] ? -1 : 1)) {
+
+               scoreboard += score[1].toString().padStart(score_pad,' ') + ' ' + score[0] + '\n'
+
+            }
+
+            ctx.replyWithHTML('<b>ACABOU!</b>\n\n'
+               + '<code>' + scoreboard + '</code>')
+
+         } else {
+
+            ctx.replyWithHTML('<b>ACABOU!</b>')
+
+         }
+
+         w = ''
+
+      }, duration)
 
    }
 
