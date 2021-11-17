79a231bdb4b6e0c0cff33b5b50c0b8a2167c08de
jrvieira <github@jrvieira.com>
2021-11-17 13:21:57 +0000

    replace Map for object

--- src/command/zodiac.js
+++ src/command/zodiac.js
@@ -5,36 +5,40 @@ module.exports = async ctx => {
 
    let query = ctx.message.text.split(' ').slice(1).join(' ')
 
-   zodiac = new Map([
-      ['aquarius','aquarius'],
-      ['aquario','aquarius'],
-      ['pisces','pisces'],
-      ['peixe','pisces'],
-      ['peixes','pisces'],
-      ['aries','aries'],
-      ['carneiro','aries'],
-      ['taurus','taurus'],
-      ['touro','taurus'],
-      ['gemini','gemini'],
-      ['gemeos','gemini'],
-      ['cancer','cancer'],
-      ['caranguejo','cancer'],
-      ['leo','leo'],
-      ['leao','leo'],
-      ['virgo','virgo'],
-      ['virgem','virgo'],
-      ['libra','libra'],
-      ['balanca','libra'],
-      ['scorpio','scorpio'],
-      ['escorpiao','scorpio'],
-      ['sagittarius','sagittarius'],
-      ['sagitario','sagittarius'],
-      ['capricorn','capricorn'],
-      ['capricornio','capricorn'],
-      ['capricornus','capricorn'],
-   ])
+   zodiac = {
+      aquarius: 'aquarius',
+      aquario: 'aquarius',
+      pisces: 'pisces',
+      peixe: 'pisces',
+      peixes: 'pisces',
+      aries: 'aries',
+      carneiro: 'aries',
+      taurus: 'taurus',
+      touro: 'taurus',
+      gemini: 'gemini',
+      gemeos: 'gemini',
+      cancer: 'cancer',
+      caranguejo: 'cancer',
+      leo: 'leo',
+      leao: 'leo',
+      virgo: 'virgo',
+      virgem: 'virgo',
+      libra: 'libra',
+      balanca: 'libra',
+      scorpio: 'scorpio',
+      escorpiao: 'scorpio',
+      sagittarius: 'sagittarius',
+      sagitario: 'sagittarius',
+      capricorn: 'capricorn',
+      capricornio: 'capricorn',
+      capricornus: 'capricorn'
+   }
 
-   let comm = 'curl -s -L https://ohmanda.com/api/horoscope/' + zodiac.get( query.normalize('NFD').replace(/\p{Diacritic}/gu, "") )
+   let q = query.normalize('NFD').replace(/\p{Diacritic}/gu, "")
+
+   if (!zodiac[q]) return null
+
+   let comm = 'curl -s -L https://ohmanda.com/api/horoscope/' + zodiac[q]
    let r
    let caller = await util.title(ctx)
 
